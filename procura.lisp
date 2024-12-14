113a0d1da7f91b46ada075665b2fffc535249b3d
João Pedro <202001541@estudantes.ips.pt>
2024-12-14 16:55:40 +0000

    bfs

--- procura.lisp
+++ procura.lisp
@@ -10,3 +10,16 @@
 #   Prof. Joaquim Filipe
 #
 |#
+
+(defun bfs (open-list closed-list goal-state successors)
+  (cond   
+    ((null open-list) nil)
+    ((equal (car open-list) goal-state) (car open-list))
+    (t
+     (let ((current-state (car open-list))
+           (next-states (funcall successors current-state)))
+       (bfs (append (remove-if (lambda (x) (member x closed-list :test #'equal)) next-states)
+                    (cdr open-list))
+            (cons current-state closed-list)
+            goal-state
+            successors)))))
