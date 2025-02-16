1ac1cdaacdee38843beaa74db1f0d3ce570dda46
ludef4r <luis.faria.martins@gmail.com>
2025-02-16 17:32:41 +0000

    BFS???????

--- procura.lisp
+++ procura.lisp
@@ -42,7 +42,7 @@
             (equal board b))
         list))
 
-(defun algorithm(tabuleiro fn-list-open)
+(defun algorithm(tabuleiro)
   "Performs the algorithm for bfs and dfs"
   (if (tabuleiro-vaziop tabuleiro) (return-from algorithm tabuleiro))
   (let ((list-open (list tabuleiro))
@@ -50,17 +50,17 @@
 	(counter 0))
     (labels
 	((recursive(open)
-	   (when (< counter 14) (format t "Open: ~a~%" open) (format t "Node: ~a~%" (first open)) (incf counter))
-;;	   (when (= counter 14) (return-from algorithm "1"))
+	   (when (< counter 3) (format t "Open: ~a~%" open) (format t "Node: ~a~%" (first open)) (incf counter))
+	   (when (= counter 3) (return-from algorithm "1"))
 	   (if (null open) (return-from recursive nil))
 	   (let* ((node (first open))
-		      (children (sexo node))
-              (solution (validate-children children)))
-         (when (not (null solution)) (return-from recursive solution))
-         ;; Use Memoization to check if any child is in list
+		  (children (sexo node))
+		  (solution (validate-children children)))
+	     (when (not (null solution)) (return-from recursive solution))
+	     ;; Use Memoization to check if any child is in list
 	     (mapcar #'(lambda(c)
-                        (when (not (check-element-in-list c open)) (setf open (funcall fn-list-open c open))))
-                    children)
+			 (when (not (check-element-in-list c open)) (setf open (append open (list c)))))
+		     children)
 	     (when (not (check-element-in-list node closed)) (push node closed))
 	     (recursive (rest open)))))
       (recursive list-open))))
