212b677c8b4ac2f2011181b713cf8cdd4e4da4b9
ludef4r <luis.faria.martins@gmail.com>
2025-02-20 22:24:37 +0000

    Player vs Bot, Bot vs Bot and Minimax With Prunning Alpha beta

---
+++ phase2/projeto.lisp
@@ -0,0 +1,13 @@
+#|
+#   O ficheiro projeto.lisp é designado para o carregamento de outros ficheiros
+#   de código, escrita e leitura de ficheiros, e o desenvolvimento da interação
+#   com o utilizador.
+#
+#   ===== PROGRAMADORES =====
+#   Lucas Alexandre S. F. de Almeida - 202100067
+#   João Pedro M. Morais - 202001541
+#
+#   ========= DOCENTE =========
+#   Prof. Joaquim Filipe
+#
+|#
